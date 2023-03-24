c8246f7a0dfeece321e79876d4f429c6819181fe
asecer79 <asecer79@DESKTOP-KCL8E4T>
2023-03-24 19:04:38 +0300

    Add project files.

---
+++ Week04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Week04
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int ageValue = 36;
+            Console.WriteLine("Hello, World!"+ageValue);
+        }
+    }
+}
+
+
+
