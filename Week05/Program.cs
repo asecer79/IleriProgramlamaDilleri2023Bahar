4de54d94aa75cd2810338f1952a3df3320bbe82f
asecer79 <asecer79@DESKTOP-KCL8E4T>
2023-03-31 18:52:44 +0300

    u

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -2,111 +2,125 @@
 
 namespace Week05
 {
-    struct Ders
+
+    class Program   
     {
-        public int dersId;
-        public string dersAdi;
-
-    }
-
-    public class Program
-    {
-        IMatematik iMatematik;
-
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //Data Types
-
-            #region 01 Value Types
-
-            #region Primitive Types
-
-            // Değer tipleri (primitif tipler ve yapılar)
-            byte byteDeger = 255;
-            sbyte sbyteDeger = -128;
-            short shortDeger = 32767;
-            ushort ushortDeger = 65535;
-            int intDeger = 42;
-            uint uintDeger = 4294967295;
-            long longDeger = 9223372036854775807;
-            ulong ulongDeger = 18446744073709551615;
-            float floatDeger = 3.14f;
-            double doubleDeger = 3.1415926535;
-            decimal decimalDeger = 3.14159265358979323846m;
-            char charDeger = 'A';
-            bool boolDeger = true;
-
-            #endregion
-
-            #region Struct Types
-
-            Ders ders1 = new Ders();
-            ders1.dersId = 1;
-            ders1.dersAdi = "İleri Prog. ";
-
-            DateTime tarihSaat = DateTime.Now;
-            TimeSpan zamanFarki = DateTime.Now - DateTime.Now.AddYears(40);
+            
 
 
-
-
-
-            #endregion
-
-            #endregion
-
-            #region 02 Reference Types
-
-            //Temel olarak 5 çeşit referans tip vardır
-
-            //1. Sınıflar
-            Araba araba0; //null 
-            Araba araba1 = new Araba();
-            araba1 = null;
-
-            //2. diziler //mutable -değişitirilebilir.
-            int[] sayilar = { 1, 2, 3 };
-            char[] i = { 'A', 'L', 'i' };
-            i[0] = 'B';
-
-
-            //string //Immutable  değiştirilemez
-            string isim = "ALİ";
-            //isim[0] = 'B';
-            isim = "BLİ";
-
-
-            // Araba[] arabalar = new Araba[] { new Araba(), new Araba(), new Araba() };
-            // List<Araba> arabalar2 = new  List<Araba>();
-
-
-            //3. Arayüz Interface
-            //IMatematik iMatematik;
-
-            //4. Delegate // Delege, temsilci
-            //public delegate void MesajGonderDelegate(string mesaj);
-
-            //5. Record (Kayıt):
-            ///public record Kisi(string Ad, string Soyad);
-
-
-            #endregion
         }
     }
 
-    class Matematikci : IMatematik
-    {
-        public float PI { get; set; }
-        public int Topla(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
 
-        public double Kuvvet(double x, double y)
-        {
-            throw new NotImplementedException();
-        }
-    }
+
+
+    //struct Ders
+    //{
+    //    public int dersId;
+    //    public string dersAdi;
+
+    //}
+
+    //public class Program
+    //{
+    //    IMatematik iMatematik;
+
+    //    private static void Main(string[] args)
+    //    {
+    //        //Data Types
+
+    //        #region 01 Value Types
+
+    //        #region Primitive Types
+
+    //        // Değer tipleri (primitif tipler ve yapılar)
+    //        byte byteDeger = 255;
+    //        sbyte sbyteDeger = -128;
+    //        short shortDeger = 32767;
+    //        ushort ushortDeger = 65535;
+    //        int intDeger = 42;
+    //        uint uintDeger = 4294967295;
+    //        long longDeger = 9223372036854775807;
+    //        ulong ulongDeger = 18446744073709551615;
+    //        float floatDeger = 3.14f;
+    //        double doubleDeger = 3.1415926535;
+    //        decimal decimalDeger = 3.14159265358979323846m;
+    //        char charDeger = 'A';
+    //        bool boolDeger = true;
+
+    //        #endregion
+
+    //        #region Struct Types
+
+    //        Ders ders1 = new Ders();
+    //        ders1.dersId = 1;
+    //        ders1.dersAdi = "İleri Prog. ";
+
+    //        DateTime tarihSaat = DateTime.Now;
+    //        TimeSpan zamanFarki = DateTime.Now - DateTime.Now.AddYears(40);
+
+
+
+
+
+    //        #endregion
+
+    //        #endregion
+
+    //        #region 02 Reference Types
+
+    //        //Temel olarak 5 çeşit referans tip vardır
+
+    //        //1. Sınıflar
+    //        Araba araba0; //null 
+    //        Araba araba1 = new Araba();
+    //        araba1 = null;
+
+    //        //2. diziler //mutable -değişitirilebilir.
+    //        int[] sayilar = { 1, 2, 3 };
+    //        char[] i = { 'A', 'L', 'i' };
+    //        i[0] = 'B';
+
+
+    //        //string //Immutable  değiştirilemez
+    //        string isim = "ALİ";
+    //        //isim[0] = 'B';
+    //        isim = "BLİ";
+
+
+    //        // Araba[] arabalar = new Araba[] { new Araba(), new Araba(), new Araba() };
+    //        // List<Araba> arabalar2 = new  List<Araba>();
+
+
+    //        //3. Arayüz Interface
+    //        //IMatematik iMatematik;
+
+    //        //4. Delegate // Delege, temsilci
+    //        //public delegate void MesajGonderDelegate(string mesaj);
+
+    //        //5. Record (Kayıt):
+    //        ///public record Kisi(string Ad, string Soyad);
+
+
+    //        #endregion
+    //    }
+    //}
+
+    //class Matematikci : IMatematik
+    //{
+    //    public float PI { get; set; }
+    //    public int Topla(int x, int y)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public double Kuvvet(double x, double y)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 
 }
 
