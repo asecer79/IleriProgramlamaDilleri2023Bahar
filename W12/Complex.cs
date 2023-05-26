b6f32c7f24f02c09fef3e08ecb6d4549322ad29d
asecer79 <asecer79@ASECERNB>
2023-05-26 19:38:51 +0300

    u

---
+++ W12/Complex.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week12
+{
+    public class Complex
+    {
+        private Complex() { }
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        private double Real { get; init; }
+        private double Imaginary { get; init; }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+
+            return new Complex()
+            {
+                Real = a.Real + b.Real,
+                Imaginary = a.Imaginary + b.Imaginary
+            };
+        }
+
+        public static Complex operator +(double a, Complex b)
+        {
+
+            return new Complex()
+            {
+               Real = b.Real+a,
+            };
+        }
+
+        public static Complex operator +(Complex a, double b)
+        {
+
+            return new Complex()
+            {
+                Real = a.Real + b,
+            };
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+
+            return new Complex()
+            {
+                Real = a.Real - b.Real,
+                Imaginary = a.Imaginary - b.Imaginary
+            };
+        }
+        public static Complex operator -(double a, Complex b)
+        {
+
+            return new Complex()
+            {
+                Real = b.Real - a,
+            };
+        }
+
+        public static Complex operator -(Complex a, double b)
+        {
+
+            return new Complex()
+            {
+                Real = a.Real - b,
+            };
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+            => new Complex(a.Real * b.Real - a.Imaginary * b.Imaginary,
+                a.Real * b.Imaginary + a.Imaginary * b.Real);
+
+        public static Complex operator *(double a, Complex b)
+        {
+
+            return new Complex()
+            {
+                Real = b.Real*a,
+                Imaginary =b.Imaginary*a
+            };
+        }
+
+        public static Complex operator *(Complex a, double b)
+        {
+            return new Complex()
+            {
+                Real = a.Real * b,
+                Imaginary = a.Imaginary * b
+            };
+        }
+
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            double real = (a.Real * b.Real + a.Imaginary * b.Imaginary) / (b.Real * b.Real + b.Imaginary * b.Imaginary);
+            double imaginary = (a.Imaginary * b.Real - a.Real * b.Imaginary) / (b.Real * b.Real + b.Imaginary * b.Imaginary);
+
+            return new Complex(real, imaginary);
+        }
+
+
+        public static bool operator ==(Complex a, Complex b)
+        {
+            return a.Real==b.Real && a.Imaginary==b.Imaginary;
+        }
+        public static bool operator !=(Complex a, Complex b)
+        {
+            return !(a == b);
+        }
+
+        //"(2,3)" =----> 2+3i
+        public static explicit operator Complex(string c)   //var c = (Complex)"(3,2)";
+        {
+            c = c.Replace(" ", "");
+            c = c.Replace("(", "").Replace(")", "");
+            double real =Convert.ToDouble(c.Split(",")[0]);
+            double imaginary = Convert.ToDouble(c.Split(",")[1]);
+
+            return new Complex(real, imaginary);
+        }
+
+
+        public override string ToString()
+        {
+            if (Imaginary == 0)
+            {
+                return $"{Real}";
+            }
+            else if (Real == 0)
+            {
+                return $"{Imaginary}i";
+            }
+            else if (Imaginary < 0)
+            {
+                return $"{Real}{Imaginary}i";
+            }
+
+            return $"{Real} + {Imaginary}i";
+        }
+    }
+}
--- W12/Program.cs
+++ W12/Program.cs
@@ -1,12 +1,349 @@
-﻿namespace Week12
+﻿using System.Collections;
+using System.Reflection.Metadata;
+
+namespace Week12
 {
+
+    public delegate void MyDelegate(string msg);
+    public delegate double MathOps(double x, double y);
+
     class Program
     {
         static void Main(string[] args)
         {
+            #region MyRegion
+
+            //var c1 = new Complex(-2, 35);  // = 1 + i 
+            //var c2 = new Complex(-2, 3);   // = 2 + 3i
+            //var c3 = new Complex(Double.Sqrt(5), 35);
+
+            //var c4 = c3 + 5;
+
+            //var c5 = 5 + c4;
+
+            //var sum = c1 + c2 - c3 * c2 / c3 - c1;
+
+            //var numc1 = (Complex)"(3,-9)";
+
+            //var numc2 = (Complex)(Console.ReadLine());
+
+            //Console.WriteLine(numc2*2);
+
+            //Console.WriteLine(numc1);
+
+            //Console.WriteLine(c1 == c2);
+
+            //Console.WriteLine((15 * (sum + c2) * (-9)) / c4);
+
+
+            //var data =new MyNumbers();
+
+            //foreach (var number in data)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+
+            //List<Person> people = new List<Person>();
+            //people.Add(new Person { Age = 61,Name = "x"});
+            //people.Add(new Person { Age = 3, Name = "Program" });
+            //people.Add(new Person { Age = 2, Name = "A" });
+            //people.Add(new Person { Age = 8, Name = "d" });
+
+            //people.Sort();
+
+            //foreach (var person in people)
+            //{
+            //    Console.WriteLine(((Person)person).Name);
+            //}
+
+            //Person p1 = new Person(){Age = 15,Name = "Ay"};
+            //p1.Dispose();
+            //MyList list = new MyList();
+            //List<int> list2 = new List<int>();
+            //list2.Add(1);
+
+
+            #endregion
             
+
+            //MyDelegate del = ShowMessage;
+
+            //del.Invoke("how are you");
+
+            //DoProcess(1,2,ShowMessage);
+
+            //DoMathOperation(2, 4, ProcessSelector(1));
+            //DoMathOperation(2, 4, Subtract);
+            //DoMathOperation(2, 4, Divide);
+            //DoMathOperation(2, 4, Product);
+
+
+            MathOps del = Sum; //Multicast delegates
+            del += Subtract;
+            del += Divide;
+            del += Product;
+
+            del.Invoke(1, 2);
+
+            Action<double, double, MathOps> dop = DoMathOperation;
+
+            dop(1, 2, Divide);
+
+
+            Action<double, double, MathOps> m = (x, y, callback) =>
+            {
+                var res = callback(x, y);
+                Console.WriteLine(res);
+            };
+
+            m(3, 4, Divide);
+
+            Func<double, double, double> f1 = Sum;
+
+
+            Func<double, double, double> f2 = (x, y) =>
+            {
+                return x + y;
+            };
+
+            Console.WriteLine(f1(3,4));
+
+        }
+
+        static void DoMathOperation(double x, double y, MathOps callback)
+        {
+            var res = callback(x, y);
+            Console.WriteLine(res);
+        }
+
+        static MathOps ProcessSelector(int n)
+        {
+            if (n==1)
+            {
+                return Sum;
+            }
+            if (n == 2)
+            {
+                return Subtract;
+            }
+            if (n == 3)
+            {
+                return Product;
+            }
+            if (n == 4)
+            {
+                return Divide;
+            }
+
+            throw new Exception("Method bulunamadı");
+        }
+
+
+        static double Sum(double x, double y)
+        {
+            return x + y;
+
+        }
+
+        static double Subtract(double x, double y)
+        {
+            return x - y;
+
+        }
+
+        static double Product(double x, double y)
+        {
+            return x * y;
+
+        }
+
+        static double Divide(double x, double y)
+        {
+            return x / y;
+
+        }
+
+        public static void DoProcess(int a, int b, MyDelegate callback)
+        {
+
+            var res = a * b;
+            callback(res.ToString());
+        }
+
+        public static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
         }
     }
+
+    //IEnumerable<T>  //generic <>
+    class MyNumbers : IEnumerable<int>
+    {
+
+        private int[] Numbers { get; set; } = new[] { 1, 5, 3, 69, 4 };
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (var number in Numbers)
+            {
+                yield return number;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+    //IComparable<T>
+    class Person : IComparable<Person>, IDisposable
+    {
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int CompareTo(Person? other)
+        {
+            return this.Age.CompareTo(other.Age);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+            }
+
+            // Free any unmanaged objects here.
+
+            disposed = true;
+        }
+    }
+
+    //ICollection<T>
+
+    class MyList : ICollection<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+    }
+
+    //IList<T>
+    class MyList2 : IList<Person>
+    {
+        public IEnumerator<Person> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Person[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+        public int IndexOf(Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person this[int index]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+    }
+
+
+
+
 }
 
 
